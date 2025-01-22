e655a0e0d7482a198fdc63280c25de61785fa470
MaryRosary95 <rosarysagai@gmail.com>
2025-01-22 21:57:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-#Guess the Number
+# Guess the Number
 import random
-computer_guess = random.randint(1, 10)
+computer_guess = random.randint(1, 10)         #Random Values of the system guess
 while True:
-    my_guess = int(input())
 
-    if my_guess < computer_guess:
+    my_guess = int(input("Enter your input:")) #User providing the input
+
+    if my_guess < computer_guess:              #Guessing loop starts
         print("Too High, Try with different number")
     elif my_guess > computer_guess:
         print("Too low, Try with different number")
     else:
         print("Your guess is correct")
         break
-
 
     
